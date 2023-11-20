8fe884737cb255d5914b3fe6b6e1cfe99e0057e6
syifacts <ss4166269@gmail.com>
2023-11-20 08:18:30 +0700

    new oop

--- oop.php
+++ oop.php
@@ -1,112 +1,110 @@
 <?php
 include("connect.php");
 
-class UserRegistration {
+class User {
     private $koneksi;
-    private $username;
-    private $password;
-    private $err;
-    private $message;
 
     public function __construct($koneksi) {
         $this->koneksi = $koneksi;
-        $this->username = "";
-        $this->password = "";
-        $this->err = "";
-        $this->message = "";
     }
 
-    public function registerUser($username, $password) {
-        $this->username = $username;
-        $this->password = $password;
+    public function register($username, $password) {
+        $err = "";
+        $message = "";
 
-        if ($this->username == '' or $this->password == '') {
-            $this->err .= "<li>Silakan masukkan username dan password</li>";
+        // Regex untuk memeriksa aturan password
+        $password_regex = '/^(?=.[A-Z])(?=.[a-z])(?=.\d)(?=.[!@#$%^&()\-_=+{};:,<.>])[A-Za-z\d!@#$%^&()\-_=+{};:,<.>.]{8,}$/';
+
+        if ($username == '' or $password == '') {
+            $err .= "<li>Silakan masukkan username dan password</li>";
         }
 
-        if (empty($this->err)) {
-            $sql1 = "SELECT * FROM user WHERE username = '$this->username'";
+        if (!preg_match($password_regex, $password)) {
+            $err .= "<li>Password harus terdiri dari minimal 8 karakter dengan kombinasi huruf besar, huruf kecil, angka, dan simbol.</li>";
+        }
+
+        if (empty($err)) {
+            $sql1 = "SELECT * FROM user WHERE username = '$username'";
             $q1 = mysqli_query($this->koneksi, $sql1);
             $r1 = mysqli_fetch_array($q1);
 
             if (!$r1) {
-                $hashedPassword = md5($this->password);
-
-                $query = "INSERT INTO user (username, password) VALUES ('$this->username', '$hashedPassword')";
+                $hashedPassword = md5($password);
+                $query = "INSERT INTO user (username, password) VALUES ('$username', '$hashedPassword')";
 
                 if (mysqli_query($this->koneksi, $query)) {
-                    $this->message = "Registrasi berhasil!";
+                    $message = "Registrasi berhasil!";
                 } else {
-                    $this->err = "Registrasi gagal: " . mysqli_error($this->koneksi);
+                    $err = "Registrasi gagal: " . mysqli_error($this->koneksi);
                 }
             } else {
-                $this->err = "Username sudah digunakan. Silakan gunakan username lain.";
+                $err = "Username sudah digunakan. Silakan gunakan username lain.";
             }
         }
-    }
 
-    public function displayForm() {
-        echo "
-        <!DOCTYPE html>
-        <html lang='en'>
-        <head>
-            <meta charset='UTF-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Register - Smart AC</title>
-            <link rel='stylesheet' href='css/bootstrap.min.css'>
-            <link rel='stylesheet' href='css/style.css'>
-            <link rel='shortcut icon' href='img/ac.png' type='image/x-icon'>
-        </head>
-        <body>
-            <div class='container-fluid mt-5'>
-                <div class='container bg-light rounded-4 p-4'>
-                <h3>Tambah user admin</h3>";
-
-        if ($this->err) {
-            echo "<ul>{$this->err}</ul>";
-        }
-
-        if ($this->message) {
-            echo "<div class='alert alert-success'>{$this->message}</div>";
-        }
-
-        echo "
-                <form method='post'>
-                    <div class='mb-3'>
-                        <label for='username' class='form-label'>Username</label>
-                        <input type='text' name='username' value='{$this->username}' class='form-control' placeholder='Masukkan Username'>
-                    </div>
-                    <div class='mb-3'>
-                        <label for='password' class='form-label'>Password</label>
-                        <input type='password' name='password' class='form-control' placeholder='Masukkan Password'>
-                    </div>
-                    <p>Back to <a href='data.php'>admin page</a></p>
-                    <button type='submit' class='btn btn-primary' name='Register'>Daftar</button>
-                </form>
-                </div>
-            </div>
-
-            <div class='container-fluid mt-5 text-center'>
-                <div class='row'>
-                    <div class='col-md'>
-                        <a href='registrasi.php'><img src='img/logo-smart.png' alt='Logo Smart AC' width='300px' class='pt-3'></a>
-                    </div>
-                </div>
-            </div>";
-
-        include("login_footer.php");
-
-        echo "
-        </body>
-        </html>";
+        return [$err, $message];
     }
 }
 
-$userRegistration = new UserRegistration($koneksi);
+$userHandler = new User($koneksi);
 
 if (isset($_POST['Register'])) {
-    $userRegistration->registerUser($_POST['username'], $_POST['password']);
-}
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$userRegistration->displayForm();
+    list($err, $message) = $userHandler->register($username, $password);
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Smart AC</title>
+    <!-- Tambahkan Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/ac.png" type="image/x-icon">
+</head>
+<body>
+    <!-- Form -->
+    <div class="container-fluid mt-5">
+        <div class="container bg-light rounded-4 p-4">
+        <?php
+        if ($err) {
+            echo "<ul>$err</ul>";
+        }
+        if ($message) {
+            echo "<div class='alert alert-success'>$message</div>";
+        }
+        ?>
+        <form method="post">
+            <div class="mb-3">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" value="<?php echo $username ?>" class="form-control" placeholder="Masukkan Username">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Masukkan Password">
+            </div>
+            <p>Back to <a href="index.php">Login</a></p>
+            <button type="submit" class="btn btn-primary" name="Register">Daftar</button>
+        </form>
+        </div>
+    </div>
+
+    <!-- Logo -->
+    <div class="container-fluid mt-5 text-center">
+        <div class="row">
+            <div class="col-md">
+                <a href="registrasi.php"><img src="img/logo-smart.png" alt="Logo Smart AC" width="300px" class="pt-3"></a>
+            </div>
+        </div>
+    </div>
+
+<?php
+    include("login_footer.php");
+?>
+</body>
+</html>
